410cd8d29bb2088f0637dec45a1e66edf37d7abe
Alexander Gilman <gilmanalex@gmail.com>
2017-01-02 11:39:50 -0800

    Start project, started app, added .gitignore.

---
+++ django_maps2/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for django_maps2 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_maps2.settings")
+
+application = get_wsgi_application()

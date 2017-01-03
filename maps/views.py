443a5d691b679fa31277c2bf57e716a9a520e0c3
Alexander Gilman <gilmanalex@gmail.com>
2017-01-02 16:41:29 -0800

    Added basic registration.

--- maps/views.py
+++ maps/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def landing(request):
+    if request.user.is_authenticated():
+        return render(request,"landing-session.html")
     return render(request,"landing.html")

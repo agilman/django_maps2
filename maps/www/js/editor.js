f23e10a762360fe7c673ce2336f6c55592791052
Alexander Gilman <gilmanalex@gmail.com>
2017-01-04 01:27:53 -0800

    Added Angular-ui-router.

--- maps/www/js/editor.js
+++ maps/www/js/editor.js
@@ -1,5 +1,19 @@
-var myApp = angular.module("myApp",['ui.bootstrap']);
+var myApp = angular.module("myApp",['ui.router','ui.bootstrap']);
+
+myApp.config(function($stateProvider){
+    $stateProvider
+	.state('advEditor',{
+	    url:'',
+	    templateUrl:'/www/partials/editor-adventures.html',
+	    controller:'advEditorController',
+	});
+});
 
 myApp.controller("mainController",['$scope','$log','$http',function($scope,$log,$http){
     $log.log("Hello from main controller");
+}]);
+
+
+myApp.controller("advEditorController",['$scope','$log','$http',function($scope,$log,$http){
+    $log.log("Hello from adv editor controller");
 }]);

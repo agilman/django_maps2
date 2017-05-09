e1585251728269270e2cb76372c37ed98d814782
Alexander Gilman <gilmanalex@gmail.com>
2017-05-09 15:54:27 -0700

    working on slider navigation

--- maps/www/app/editor/components/photoEditor/albumEditor.js
+++ maps/www/app/editor/components/photoEditor/albumEditor.js
@@ -3,10 +3,13 @@ myApp.controller("photoEditorAlbumController",['$scope','$log','$http','$statePa
     $scope.albumId = $stateParams.albumId;
     $scope.slickLoaded = false;
     $scope.pictures = [];
-        
+    $scope.sliderIndex = null;
     $http.get('/api/rest/pictures/' + $scope.albumId+"/").then(function(data){
 	$scope.pictures = data.data;
-	$scope.slickLoaded = true;
+	$timeout(function () {
+	    $scope.slickLoaded = true;
+	}, 10);
+
     });    
     
     $scope.uploadClick = function(){
@@ -29,37 +32,77 @@ myApp.controller("photoEditorAlbumController",['$scope','$log','$http','$statePa
 	    }).then(function(data){
 		$scope.slickLoaded = false;
 		$scope.pictures.push(data.data);
-
+		
 		$timeout(function () {
 		    $scope.slickLoaded = true;
-		}, 5);
+		}, 10);
 		
-		
+		$timeout(function () {
+		    $scope.slideConfig.method.slickGoTo($scope.pictures.length-1);
+		},100);
 	    });
-	}
-
-
-	
+	}	
     };
     
     $scope.slideConfig = {
 	lazyLoad: 'ondemand',
 	rows: 1,
-	dots: true,
+	dots:true,
 	autoplay: false,
 	infinite: true,
-	slidesToShow: 6,
-	slidesToScroll: 3,
+	slidesToShow:6,
+	slidesToScroll:5,
+	centerMode: true,
 	method:{},
 	event: {
-	    beforeChange: function (event, slick, currentSlide, nextSlide) {
+	    init: function (event, slick) {
+		//
 	    },
 	    afterChange: function (event, slick, currentSlide, nextSlide) {
+		//track index on change.
+		$scope.sliderIndex=currentSlide;
+	    },
+	    swipe: function(event,slick,direction){
+		$log.log("swipe",direction);
+		if(direction=="right"){
+		    $log.log("going right");
+		    slick.slickGoTo(-5);
+		}else if (direction=="left"){
+		    $log.log("going left");
+		}
+	    }   
+	},
+	responsive: [
+	    {
+		breakpoint: 1024,
+		settings: {
+		    slidesToShow: 5,
+		    slidesToScroll: 5,
+		    infinite: true,
+		    dots: true
+		}
+	    },
+	    {
+		breakpoint: 600,
+		settings: {
+		    slidesToShow: 4,
+		    slidesToScroll: 2
+		}
 	    }
-	}
-	
+	]
     };
     
+    $scope.sliderGoLeft = function(){
+	if($scope.sliderIndex<5){
+	    $scope.slideConfig.method.slickGoTo($scope.pictures.length-1-(5-$scope.sliderIndex));
+	}
+
+	$scope.slideConfig.method.slickGoTo($scope.sliderIndex-5);	
+    };
+
+    $scope.sliderGoRight = function(){
+	$scope.slideConfig.method.slickGoTo($scope.sliderIndex+5);
+    };
     
     $log.log("Hello from photoAlbumController");    
 }]);

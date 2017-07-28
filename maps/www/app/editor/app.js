b83f9b205470ab0564b29297501e3773c2dbf51d
Alexander Gilman <gilmanalex@gmail.com>
2017-07-27 22:59:40 -0700

    added textAngular - a wysiwyg editor.

--- maps/www/app/editor/app.js
+++ maps/www/app/editor/app.js
@@ -1,1 +1,1 @@
-var myApp = angular.module("myApp",['ui.router','ui.bootstrap','ui.bootstrap.datetimepicker','leaflet-directive','slickCarousel','angularSpinner']);
+var myApp = angular.module("myApp",['ui.router','ui.bootstrap','ui.bootstrap.datetimepicker','leaflet-directive','slickCarousel','angularSpinner','textAngular']);

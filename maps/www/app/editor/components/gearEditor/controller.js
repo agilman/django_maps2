c45263bc025b52301f6d116643bd6e65e2878711
Alexander Gilman <gilmanalex@gmail.com>
2017-08-09 15:50:54 -0700

    Moved all css and js offline. Added angular-ui-tree. Needs more work. Demo doesn't work.

--- maps/www/app/editor/components/gearEditor/controller.js
+++ maps/www/app/editor/components/gearEditor/controller.js
@@ -1,5 +1,55 @@
 myApp.controller("gearEditorController",['$scope','$log','$http','$stateParams',function($scope,$log,$http, $stateParams){
     $scope.$emit("setGearEditorActive");
-
+    
+    $scope.list = [{
+	'id': 1,
+	'title': 'node1',
+	'nodes': [
+	    {
+		'id': 11,
+		'title': 'node1.1',
+		'nodes': [
+		    {
+			'id': 111,
+			'title': 'node1.1.1',
+			'nodes': []
+		    }
+		]
+	    },
+	    {
+		'id': 12,
+		'title': 'node1.2',
+		'nodes': []
+	    }
+	]
+    }, {
+	'id': 2,
+	'title': 'node2',
+	'nodrop': true, // An arbitrary property to check in custom template for nodrop-enabled
+	'nodes': [
+	    {
+		'id': 21,
+		'title': 'node2.1',
+		'nodes': []
+	    },
+	    {
+		'id': 22,
+		'title': 'node2.2',
+		'nodes': []
+	    }
+	]
+    }, {
+	'id': 3,
+	'title': 'node3',
+	'nodes': [
+	    {
+		'id': 31,
+		'title': 'node3.1',
+		'nodes': []
+	    }
+	]
+    }];
+    
+    
     $log.log("Hello from Gear editor controller");
 }]);

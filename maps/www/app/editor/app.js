8805fd13c80d1ad104410ea3fbd9a950895135d8
Alexander Gilman <gilmanalex@gmail.com>
2017-08-10 17:03:01 -0700

    Moved from Angular-ui-tree to ng-js-tree.

--- maps/www/app/editor/app.js
+++ maps/www/app/editor/app.js
@@ -1,1 +1,1 @@
-var myApp = angular.module("myApp",['ui.router','ui.bootstrap','ui.bootstrap.datetimepicker','leaflet-directive','slickCarousel','angularSpinner','textAngular','ui.tree']);
+var myApp = angular.module("myApp",['ui.router','ui.bootstrap','ui.bootstrap.datetimepicker','leaflet-directive','slickCarousel','angularSpinner','textAngular','ngJsTree']);

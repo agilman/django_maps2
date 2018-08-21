a5c56590cbd526dc10d3b369bfe78ea835d2c4d8
Your Mom <gilmanalex@gmail.com>
2018-08-21 13:03:45 -0700

    Rendering the visited users' Id in template.

--- maps/views.py
+++ maps/views.py
@@ -28,9 +28,13 @@ def registration(request):
 
                         
 def getUserIdFromUserName(userName):
-    user = User.objects.get(username=userName)
-    #TODO error handling...
-    return user.pk
+    try:
+        user = User.objects.get(username=userName)
+        return user.pk
+    except User.DoesNotExist:
+        return 0
+    
+    
 
 # Create your views here.
 
@@ -41,14 +45,23 @@ def landing(request):
 
 
 def profileViewer(request,userName):
-    if request.user.is_authenticated():
+    userId = getUserIdFromUserName(userName)
 
-        return render(request,"profile-viewer-session.html")
+    context = {'userId':userId,'userName':userName}
 
-    return render(request,"profile-viewer.html")
+    if userId:
+        if request.user.is_authenticated():
+
+            return render(request,"profile-viewer-session.html",context)
+
+        return render(request,"profile-viewer.html",context)
+    else:
+        return HttpResponse("User does not exist.")
 
 def editorViewer(request):
-    context = {'mapboxToken': settings.MAPBOX_TOKEN}
+    context = {'mapboxToken': settings.MAPBOX_TOKEN,
+               'userId':request.user.pk,
+               'userName': request.user.username}
     
     if request.user.is_authenticated():
 

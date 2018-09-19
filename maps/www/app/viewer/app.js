61f7f9ee90171c556beebb9bef6a106a1fef3997
Your Mom <gilmanalex@gmail.com>
2018-09-19 14:14:17 -0700

    Resized picture in preview to fit width. Updated angularjs, leaflet, jquery.

--- maps/www/app/viewer/app.js
+++ maps/www/app/viewer/app.js
@@ -1,1 +1,9 @@
+angular.lowercase=function(text){  //this is a workaround to removed function... https://stackoverflow.com/questions/50448326/uncaught-typeerror-angular-lowercase-is-not-a-function
+    if (text){
+	return text.toLowerCase();
+    }else{
+	return null;
+    }
+}
+
 var myApp = angular.module("myApp",['ui.router','ui.bootstrap','slickCarousel','leaflet-directive']);
